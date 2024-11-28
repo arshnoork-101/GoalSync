ad6ed9ff101f811b3f22a5813908a13cfb9c9455
Arshnoor Kaur <arshnoork3110@gmail.com>
2024-11-27 22:13:02 -0500

    Local Server Changed

--- myServer.js
+++ myServer.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Start the Server
-const port=2002;
+const port=2003;
 app.listen(port, function () {
     console.log(`Server Started on Port ${port}`);
 });
